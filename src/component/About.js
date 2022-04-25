04011b1c78c1b07c9fffd0ab8f271049c5b081b2
unknown <affatatiej@gmail.com>
2022-04-24 22:52:09 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/component/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+function About () {
+    return <div>
+        <h2>If youc check the code of the page, you will find an example on how to use the react-router-dom component!</h2>
+ 
+        Read more about it at :
+        <a href="https://v5.reactrouter.com/web/guides/quick-start">
+            https://v5.reactrouter.com/web/guides/quick-start
+        </a>
+    </div>
+}
+export default About;
